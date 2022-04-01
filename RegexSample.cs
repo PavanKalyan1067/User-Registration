d71e5b66e0a6a4f8dc89d739e7f4fe56a89d7b5c
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-01 12:05:15 +0530

    Validating Last Name

--- RegexSample.cs
+++ RegexSample.cs
@@ -14,8 +14,12 @@ namespace UserRegisteration
         public void Validating()
         {
             Regex regex = new Regex(pattern);
+
             Console.WriteLine("Validating the first name");
             ValidatingFirstName();
+
+            Console.WriteLine("Validating The Last Name");
+            ValidatingLastName();
 
         }
 
@@ -33,6 +37,23 @@ namespace UserRegisteration
             {
                 Console.WriteLine("Invalid");
             }
+        }
+
+        public void ValidatingLastName()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Enter the last name : ");
+            string input1 = Console.ReadLine();
+            bool res1 = regex.IsMatch(input1);
+            if (res1)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
         }
     }
 }
